452239d4a8fefd784ef5c9f125fffd259d70dbd1
gannbarejustin <gannbarejustin@gmail.com>
2017-03-01 22:35:38 +0800

    backup

--- forex.py
+++ forex.py
@@ -1,7 +1,13 @@
 #!python
-
+# -*- coding: utf-8 -*-
 import pandas as pd
-url="http://rate.bot.com.tw/xrt?Lang=zh-TW"
+##url="http://rate.bot.com.tw/xrt?Lang=zh-TW"
+##
+##df = pd.read_html(url)
+##
+##f = open('forex.html', 'w')
+##f.write(df[0].to_html())
 
-df = pd.read_html(url)
-print(df[0])
+df = pd.read_html('forex.html')
+
+print(df[0][0][0])
