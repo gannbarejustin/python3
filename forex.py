71db8b8c4b5edfe549de8d1ca5ab3e6fd057ab43
justin <gannbarejustin@gmail.com>
2017-03-02 18:20:37 +0800

    backup

--- forex.py
+++ forex.py
@@ -1,13 +1,23 @@
 #!python
 # -*- coding: utf-8 -*-
 import pandas as pd
-##url="http://rate.bot.com.tw/xrt?Lang=zh-TW"
-##
-##df = pd.read_html(url)
-##
-##f = open('forex.html', 'w')
-##f.write(df[0].to_html())
+import codecs
+url="http://rate.bot.com.tw/xrt?Lang=zh-TW"
 
-df = pd.read_html('forex.html')
+df = pd.read_html(url)
 
-print(df[0][0][0])
+f = codecs.open('forex.html', 'w', "utf-8") #write unicode to file
+f.write(df[0].to_html())
+
+df = pd.read_html('forex.html', encoding = 'utf8')
+f.close()
+
+kk = df[0] #kk is a DataFrame
+kk = kk[[u'幣別', u'幣別.1', u'現金匯率', 'Unnamed: 3', u'即期匯率']]
+kk.columns = [u'幣別',u'現金買入',u'現金賣出',u'即期買入',u'即期賣出']
+print(kk)
+
+#df=df[0]
+#print(df[0])
+
+#df.drop(df.columns[[5,6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]], axis=1) #axis=1 denote column
--- pandasBasic2.py
+++ pandasBasic2.py
@@ -9,12 +9,15 @@ fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._stat
 
 #This is a list
 print(fiddy_states)
+print("###############")
 
 #This is a dataframe
 print(fiddy_states[0])
+print("###############")
 
 #This is a column
 print(fiddy_states[0][0])
+print("###############")
 
 for abbv in fiddy_states[0][0][1:]:
     print('FMAC/HPI_'+abbv)
